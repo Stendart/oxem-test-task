642adf04c83ab3cd600bd8f5c6d10758de21d92a
Stendart <Reiden1998@yandex.ua>
2021-03-29 20:37:12 +0300

    add action test

--- src/tests/table.test.js
+++ src/tests/table.test.js
@@ -1,10 +1,16 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 
 import Table from '../views/Table'
 
 const localVue = createLocalVue()
+
 localVue.use(Vuex)
+localVue.use(VueRouter)
+
+const router = new VueRouter()
+
 
 const testPeopleList = [
   {
@@ -41,25 +47,34 @@ const tableHead = ['id', 'firstName', 'lastName', 'email', 'phone']
 
 describe('Table', () => {
   let getters
+  let actions
   let store
   beforeEach(() => {
     getters = {
       getPeoples: () => testPeopleList,
-      getFilterSubstr: () => 'input'
+      //getFilterSubstr: () => 'input'
+    }
+    actions = {
+      getData: jest.fn(),
     }
 
     store = new Vuex.Store({
-      getters
+      getters,
+      actions
     })
   })
 
   test('getter should return equal data', () => {
-    const wrapper = shallowMount(Table, { store, localVue })
+    const wrapper = shallowMount(Table, { store, localVue, router })
     expect(wrapper.vm.peopleList).toStrictEqual([getters.getPeoples()])
+  })
+  test('action getData have been called', () => {
+    const wrapper = shallowMount(Table, { store, localVue, router })
+    expect(actions.getData).toHaveBeenCalled()
   })
 
   test('sould initialize corectly', () => {
-    const wrapper = shallowMount(Table, { store, localVue })
+    const wrapper = shallowMount(Table, { store, localVue, router })
     expect(wrapper.is(Table)).toBeTruthy()
     expect(wrapper.vm.tableHead).toEqual(tableHead)
     expect(wrapper.vm.countElemInPatr).toEqual(50)
